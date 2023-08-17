f17a9c470ca25ec22ae1aab38d09c81352e2210a
Traap <gary.a.howard@mac.com>
2023-08-17 11:27:39 -0700

    Character example.

---
+++ 03-CustomTypes/character.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let c = 'z';
+    println!("c = {c}");
+
+    let z: char = 'ℤ'; // with explicit type annotation
+    println!("z = {z}");
+
+    let heart_eyed_cat = '😻';
+    println!("heart_eyed_cat = {heart_eyed_cat}");
+}
