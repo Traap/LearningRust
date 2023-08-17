d279dde2a4f5d7ee965792526eb30aff021e322b
Traap <gary.a.howard@mac.com>
2023-08-17 11:28:05 -0700

    Mathematical example.

---
+++ 03-CustomTypes/mathematical.rs
@@ -0,0 +1,24 @@
+fn main() {
+    // addition
+    let sum = 5 + 10;
+    println!("sum = {sum}");
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+    println!("difference = {difference}");
+
+    // multiplication
+    let product = 4 * 30;
+    println!("product = {product}");
+
+    // division
+    let quotient = 56.7 / 32.2;
+    println!("quotient = {quotient}");
+
+    let truncated = -5 / 3;
+    println!("truncated = {truncated}");
+
+    // remainder
+    let remainder = 43 % 5;
+    println!("remainder = {remainder}");
+}
